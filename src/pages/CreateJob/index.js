52e28d287315adfc7d4f484aa4f8228798ef7381
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-05-27 22:56:21 -0300

    pages created :tada:

---
+++ src/pages/CreateJob/index.js
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+function CreateJob() {
+  return (
+    <Container>
+      <h1>CreateJob</h1>
+    </Container>
+  );
+};
+
+export default CreateJob;
---
+++ src/pages/EditJob/index.js
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+function EditJob() {
+  return (
+    <Container>
+      <h1>EditJob</h1>
+    </Container>
+  );
+};
+
+export default EditJob;
